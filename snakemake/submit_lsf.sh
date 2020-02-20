8cf2df885a1beba56439602d39142884b6ed8b57
leandro <leandro@ebi.ac.uk>
2020-02-20 12:59:25 +0000

    Not using the lsf profile nor singularity anymore
    calling singularity in the shell command itself

--- snakemake/submit_lsf.sh
+++ snakemake/submit_lsf.sh
@@ -2,7 +2,6 @@
 JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
 LOG_DIR=logs/
 MEMORY=8000
-PROFILE="lsf"
 
 mkdir -p $LOG_DIR
 
@@ -11,6 +10,6 @@ bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
     -o "$LOG_DIR"/"$JOB_NAME".o \
     -e "$LOG_DIR"/"$JOB_NAME".e \
     -J "$JOB_NAME" \
-      snakemake -p --use-singularity --profile "$PROFILE" "$@"
+      snakemake -p "$@"
 
 exit 0
